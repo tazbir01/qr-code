5db4aeeadf3d2db345a6726f2d4396944f480642
tazbir01 <tajberahamad@gmail.com>
2025-11-19 23:14:33 +0600

    added qrcode

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const handleGenerateCode = () => {
-    console.log(" hellow ")
+    const url = document.getElementById("url").value.trim()
+    // trim() for white space remove 
+    if (!url) {
+        return alert("Provide  valid url")
+    }
+    document.getElementById("url").value = "";
+
+    try{
+
+        new QRCode(document.getElementById("QR-code"),{
+            text: url,
+            width: 200,
+            height: 200,
+            colorDark: "#000000",
+            colorLight: "#ffffff"
+        })
+    }
+    catch(error){
+        console.log(error)
+    }
 }
